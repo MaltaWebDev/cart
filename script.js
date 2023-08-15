0d101ea0111071a80a8d34ed03b548701976b9e5
MaltaWebDev <MaltaWebDev@gmail.com>
2023-08-15 12:07:34 +0200

    Add cart functionality

--- script.js
+++ script.js
@@ -4,23 +4,30 @@ const itemsContainer = document.querySelector("#items-container");
 const cartContainer = document.querySelector(".cart");
 const btn = document.querySelectorAll(".btn");
 
+// create products as arr of objects
 const products = [
-  { id: 1, itemNumber: "Item 1", price: "Price 60", url: "./img/bcaa.jpg" },
-  { id: 2, itemNumber: "Item 2", price: "Price 80", url: "./img/creatine.jpg" },
+  { id: 1, itemNumber: "Item 1", price: "Price $60", url: "./img/bcaa.jpg" },
+  {
+    id: 2,
+    itemNumber: "Item 2",
+    price: "Price $80",
+    url: "./img/creatine.jpg",
+  },
   {
     id: 3,
     itemNumber: "Item 3",
-    price: "Price 10",
+    price: "Price $10",
     url: "./img/protein_powder.jpg",
   },
   {
     id: 4,
     itemNumber: "Item 4",
-    price: "Price 45",
+    price: "Price $45",
     url: "./img/fat_burners.jpg",
   },
 ];
 
+// add products to container
 products.forEach((product) => {
   itemsContainer.insertAdjacentHTML(
     "beforeend",
@@ -38,9 +45,40 @@ products.forEach((product) => {
   );
 });
 
+// add event listener to add items to cart
 itemsContainer.addEventListener("click", (e) => {
   if (e.target.id.startsWith("product")) {
     const id = parseInt(e.target.id.split("-")[1]);
     const product = products.find((product) => product.id === id);
+    console.log("btn clicked", e.target.id, product);
+    cartContainer.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="cart-item">
+        <p class="item-number">${product.itemNumber}</p>
+        <p class="price">${product.price}</p>
+        <p class="quantity">Quantity: <span>1</span></p>
+        <button class="minus-btn" data-product-id="${product.id}">-</button>
+        <button class="plus-btn" data-product-id="${product.id}">+</button>
+      </div>`
+    );
+  }
+});
+
+// add functionality to plus and minus buttons
+cartContainer.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("minus-btn") ||
+    e.target.classList.contains("plus-btn")
+  ) {
+    const cartItem = e.target.closest(".cart-item");
+    const quantityElement = cartItem.querySelector(".quantity span");
+    let currentQuantity = parseInt(quantityElement.textContent);
+    if (e.target.classList.contains("minus-btn") && currentQuantity > 1) {
+      currentQuantity--;
+    } else if (e.target.classList.contains("plus-btn")) {
+      currentQuantity++;
+    }
+    quantityElement.textContent = currentQuantity;
   }
 });
